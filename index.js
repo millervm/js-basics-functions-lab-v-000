4cc097f68ad0e7c0191beb71ca4d053b5f5437ee
Veronica Miller <vmcmiller@gmail.com>
2018-09-30 06:10:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function distanceFromHqInFeet(blocks) {
   return distanceFromHqInBlocks(blocks) * 264;
 }
 
-function distanceTravelledInFeet(block1, block2) {
-  return Math.abs(block1 - block2) * 264;
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  
 }
