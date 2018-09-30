0e9e67333f74e2b1389ffa6f1c4348e7a062390d
Veronica Miller <vmcmiller@gmail.com>
2018-09-30 06:18:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function calculatesFarePrice(start, destination) {
   if (Math.abs(start - destination) < 400) {
     price = 0;
   }
-  else if (Math.abs(start - destination) >= 400 && Math.abs(start - destination) <= 2000) {
-    price = distanceTravelledInFeet(start, destination)) * 0.02;
+  else if distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+    price = distanceTravelledInFeet(start, destination) * 0.02;
   }
 }
